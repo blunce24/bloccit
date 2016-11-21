ff22f1688fceb543ede2837c9ee1563d6d196427
blunce24 <blairlunceford@gmail.com>
2016-11-20 20:48:04 -0600

    Completed Checkpoint 27

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts
   #register inline callback after the before_save callback
   before_save { self.email = email.downcase if email.present? }
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
